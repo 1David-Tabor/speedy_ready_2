2a430b2b729c3173c239e48d741c929aba17a50b
David Tabor <1davidtabor1@gmail.com>
2022-06-11 13:31:57 -0700

    boilerplate tkinter

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 #David
+import tkinter as tk
+import tkinter.ttk as ttk
+
+class SpeedyReady:
+    def __init__(self, master):
+        self.master = master
+        frame = ttk.Frame(self.master)
+        style = ttk.Style()
+        style.configure("Custom.TLabel",foreground="white",
+                                        background="red",
+                                        padding=[10, 10, 10, 10],
+                                        relief = "raised")
+        label = ttk.Label(frame, text = "Hello World", style = "Custom.TLabel")
+        label.pack(padx = 5, pady = 5)
+        frame.pack(padx = 5, pady = 5)
+
+root = tk.Tk()
+root.geometry("200x150")
+window = SpeedyReady(root)
+root.mainloop()
