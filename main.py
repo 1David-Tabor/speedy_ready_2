5192af63a1c15228240ee0eedbda37d0d77c9e77
David Tabor <1davidtabor1@gmail.com>
2022-06-11 14:01:42 -0700

    changed styling and removed ttk.

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 #David
 import tkinter as tk
-from tkinter import font
-import tkinter.ttk as ttk
 #words = re.sub("[^\w]", " ",  mystr).split()
 #Converts string to list of words seperated by spaces.
 class SpeedyReady:
     def __init__(self, master):
         self.master = master
-        frame = ttk.Frame(self.master)
+        frame = tk.Frame(self.master)
         self.text0 = tk.StringVar()
         self.text1 = tk.StringVar()
         self.text2 = tk.StringVar()
-        style = ttk.Style()
-        style.configure("Custom.TLabel",foreground="white",
-                                        background="red",
-                                        padding=[10, 10, 10, 10],
-                                        font=("Helvetica", 25),
-                                        relief = "raised")
-        label0 = ttk.Label(frame, text=self.text0, style="Custom.TLabel")
+        myFont = ("Arial", 30)
+        label0 = tk.Label(frame, font=myFont, text=self.text0)
         label0.pack(padx = 5, pady = 5)
-        label1 = ttk.Label(frame, text=self.text1, style="Custom.TLabel")
+        label1 = tk.Label(frame, font=myFont, text=self.text1)
         label1.pack(padx = 5, pady = 5)
-        label2 = ttk.Label(frame, text=self.text2, style="Custom.TLabel")
+        label2 = tk.Label(frame, font=myFont, text=self.text2)
         label2.pack(padx = 5, pady = 5)
         frame.pack(padx = 5, pady = 5)
 
